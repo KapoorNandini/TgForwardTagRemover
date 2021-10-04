df34f578f239ba145f58d18012b915ff3857d2f1
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2021-10-04 16:22:36 +0530

    Update ftr.py

--- ftr.py
+++ ftr.py
@@ -1,9 +1,9 @@
 # Anonymize - Forward tag remover bot
-# Copyright (C) 2021 @Alain_xD
+# Copyright (C) 2021 @IshanSingla
 #
-# This file is a part of < https://github.com/BotzCity/ForwardTagRemover/ >
+# This file is a part of < https://github.com/IshanSingla/TgForwardTagRemover/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/BotzCity/ForwardTagRemover/blob/main/LICENSE/>.
+# <https://www.github.com/IshanSingla/TgForwardTagRemover/blob/main/LICENSE/>.
 
 import re, os, random, asyncio, html
 os.system("pip install pyrogram")
@@ -17,17 +17,17 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = pyrogram.Client("app", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-but = InlineKeyboardMarkup([[InlineKeyboardButton("Help 🤔", callback_data="Help"), InlineKeyboardButton("Close 🔐", callback_data="close")],[InlineKeyboardButton("Updates ⬆", url="t.me/BotzCity"), InlineKeyboardButton("Developer 💕", url="t.me/Alain_xD")]])
-butt = InlineKeyboardMarkup([[InlineKeyboardButton("Home 🏠", callback_data="home"), InlineKeyboardButton("Updates ⬆", url="t.me/BotzCity")], [InlineKeyboardButton("Repo 👉", url="GitHub.com/BotzCity/ForwardTagRemover"), InlineKeyboardButton("Close 🔐", callback_data="close")]])
+but = InlineKeyboardMarkup([[InlineKeyboardButton("Help 🤔", callback_data="Help"), InlineKeyboardButton("Close 🔐", callback_data="close")],[InlineKeyboardButton("Updates ⬆", url="https://t.me/InducedBotUpdates"), InlineKeyboardButton("Developer 💕", url="t.me/IshanSingla")]])
+butt = InlineKeyboardMarkup([[InlineKeyboardButton("Home 🏠", callback_data="home"), InlineKeyboardButton("Updates ⬆", url="https://t.me/InducedBotUpdates")], [InlineKeyboardButton("Repo 👉", url="GitHub.com/IshanSingla/TgForwardTagRemover"), InlineKeyboardButton("Close 🔐", callback_data="close")]])
 
 
 @app.on_message(filters.command(["start"]))
 async def start(lel, message):
-    await message.reply_text(f"**Hi** `{message.from_user.first_name}` **!\n\nI'm any forward tag remover // anonymize bot! I can send the file which you sended to me, without forward tag..!**", reply_markup=but)
+    await message.reply_text(f"**Hi** `{message.from_user.first_name}` **!\n\nI'm any forward tag remover // anonymize bot! I can send the file which you sended to me, without forward tag..!\nMade with ❤️ By Induced**", reply_markup=but)
 
 @app.on_message(filters.command(["help"]))
 async def help(ha, message):
-    await app.send_message(message.chat.id, """**There is nothing no more..!\nJust forward me any file // message and I will return it back without forwaded tag.\n\nMade with ❤️ by @BotzCity**""", reply_markup=butt) 
+    await app.send_message(message.chat.id, """**There is nothing no more..!\nJust forward me any file // message and I will return it back without forwaded tag.\n\nMade with ❤️ By Induced**""", reply_markup=butt) 
 
 @app.on_callback_query()
 async def button(app, update):
